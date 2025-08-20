46d6a8bd905ce4268f56087f03eeac546b84b28a
LuvForAirplanes <micahstauffer04@gmail.com>
2025-08-20 13:22:25 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Telerik.Reporting;
+
+Console.WriteLine("Hello, World!");
+
+var reportPackager = new ReportPackager();
+using var sourceStream = new MemoryStream();
+reportPackager.Package(File.ReadAllText("/opt/candle-server/Candle/Candle.Services/SeedData/Reports/Report Cards/Citrus Report Card.trdx"), sourceStream);
